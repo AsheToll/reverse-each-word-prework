8497e6526f98d65a70762d52ef36d792b921a2c9
Ashley Tolen <tolenashley7@gmail.com>
2019-05-23 21:37:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,18 @@
-require 'pry'
-def reverse_each_word(string)
-  # turn into and save as array to work on
-  string_array = string.split
+# require 'pry'
+# def reverse_each_word(string)
+#   # turn into and save as array to work on
+#   string_array = string.split
   
-  # for storing the new string
-  new_string = []
+#   # for storing the new string
+#   new_string = []
   
-  string_array.each do |word|
-    new_string << word.reverse
-  end
+#   string_array.each do |word|
+#     new_string << word.reverse
+#   end
   
-  # turn back into a string
-  new_string.join(" ")
-end
+#   # turn back into a string
+#   new_string.join(" ")
+# end
 
 def reverse_each_word(string)
   
